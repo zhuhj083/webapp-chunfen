19368a9c4eecb2de41a768b1cf9b19b0e7d3ff4a
thzhuhjhj_bijiben <zhuhaijun1988@qq.com>
2018-06-26 22:43:45 +0800

    'changge'

---
+++ src/main/java/com/test/concurrency/atomic/AtomicIntegerArrayTest.java
@@ -0,0 +1,14 @@
+package com.test.concurrency.atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+public class AtomicIntegerArrayTest {
+    static int[] value = new int[]{1,2};
+    static AtomicIntegerArray ai = new AtomicIntegerArray(value);
+
+    public static void main(String[] args) {
+        ai.getAndSet(0,3);
+        System.out.println(ai.get(0));
+        System.out.println(value[0]);
+    }
+}
