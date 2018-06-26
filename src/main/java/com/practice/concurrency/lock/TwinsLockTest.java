359b520fe7afe3803c7a3a960a8afb1d7fdc9a23
zhuhaijun <zhuhaijun1988@qq.com>
2018-06-26 20:08:56 +0800

    change package name

---
+++ src/main/java/com/practice/concurrency/lock/TwinsLockTest.java
@@ -0,0 +1,42 @@
+package com.practice.concurrency.lock;
+
+import com.practice.util.SleepUtils;
+import org.junit.Test;
+
+import java.util.concurrent.locks.Lock;
+
+public class TwinsLockTest {
+    @Test
+    public void test(){
+        final Lock lock = new TwinsLock();
+        class Worker extends  Thread{
+            @Override
+            public void run() {
+                while(true){
+                    lock.lock();
+                    try {
+                        SleepUtils.second(1);
+                        System.out.println(Thread.currentThread().getName());
+                        SleepUtils.second(1);
+                    }finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+
+        //启动10个线程
+        for (int i = 0; i < 10; i++) {
+            Worker w = new Worker();
+            w.setDaemon(true);
+            w.start();
+        }
+
+        //每个1秒换行
+        for (int i = 0; i < 10; i++) {
+            SleepUtils.second(1);
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/main/java/com/practice/concurrency/threadPool/ThreadPoolTest.java
@@ -0,0 +1,52 @@
+package com.practice.concurrency.threadPool;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by zhj on 2018/6/26.
+ */
+
+public class ThreadPoolTest {
+
+    private static  final int CPU_SIZE = Runtime.getRuntime().availableProcessors();
+    private static BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(10);
+
+    static class MyThreadPoolExecutor extends ThreadPoolExecutor{
+
+        public MyThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        }
+
+        @Override
+        protected void beforeExecute(Thread t, Runnable r) {
+            System.out.println(t.getName() + " will execute this task,");
+        }
+
+        @Override
+        protected void afterExecute(Runnable r, Throwable t) {
+            System.out.println("afterExecute");
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor pool = new MyThreadPoolExecutor(2*CPU_SIZE,4*CPU_SIZE,1, TimeUnit.DAYS,queue);
+
+        ExecutorService ex = Executors.newFixedThreadPool(34);
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(3);
+
+        Thread t = new Thread();
+
+        Future<Integer> future = pool.submit(t,122);
+        try {
+            System.out.println("future="+ future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end");
+        pool.shutdown();
+    }
+}
